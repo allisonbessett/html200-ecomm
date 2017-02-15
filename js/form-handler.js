668fca34db61cc344acecbb841df16d41924e853
Allison Bessett <allison_17@hotmail.com>
2017-02-15 15:48:44 -0800

    Console is printing correctly.

--- js/form-handler.js
+++ js/form-handler.js
@@ -1,4 +1,6 @@
 function capture(){
-  console.log(document.filter.name.value);
-  event.preventDefault;
+  console.log("in the func");
+  console.log(document.custInfo.name.value);
+  console.log(document.custInfo.age.value);
+  event.preventDefault();
 }
